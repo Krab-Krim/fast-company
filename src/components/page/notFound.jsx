6fa7dac411ce162cf5e60c8eb4e19977f6f4534f
VeraVK <pvk@git.one-touch.ru>
2022-05-11 18:27:08 +0300

    Задание 10. Формы. Часть 2 - работа с NotFound

--- src/components/page/notFound.jsx
+++ src/components/page/notFound.jsx
@@ -1,14 +1,38 @@
 import React from "react";
+import pictures from "../../statics/images/images.png";
+import { useHistory } from "react-router-dom";
 
 const NotFound = () => {
+    document.body.style.backgroundImage = `url(${pictures})`;
+
+    const history = useHistory();
+
+    const handleSave = () => {
+        history.push(`/`);
+    };
     return (
         <>
-            <h1 className="display-1 text-center">Ошибка 404</h1>
-            <p className="lead">
-                Кажется что-то пошло не так! Страница, которую вы запрашиваете, не существует. Возможно
-                она устарела, была удалена, или быд введен неверный адрес в адресной строке.
-            </p>
-            <button>Перейти на главную</button>
+            <div className="container mt-5">
+                <div>
+                    <div className="mb-4">
+                        <h1 className="display-1 text-center ">Ошибка 404</h1>
+                    </div>
+                    <div className="mb-4">
+                        <p className="lead text-center">
+                            Кажется что-то пошло не так! Страница, которую вы запрашиваете, не существует. Возможно
+                            она устарела, была удалена, или быд введен неверный адрес в адресной строке.
+                        </p>
+                    </div>
+                    <div className="d-flex justify-content-center">
+                        <button
+                            className="btn btn-primary w-50"
+                            onClick={() => handleSave()}
+                        >
+                            Перейти на главную
+                        </button>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
