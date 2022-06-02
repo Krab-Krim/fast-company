78a2b04bfe9abe9b36b14389ce69fd201e8c6534
VeraVK <pvk@git.one-touch.ru>
2022-06-02 10:50:32 +0300

    15. Firebase и трансформеры данных

--- src/layouts/main.jsx
+++ src/layouts/main.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import pictures from "../statics/images/images.png";
+import useMockData from "../utils/mockData";
 
 const Main = () => {
+    const { error, initialize, progress, status } = useMockData();
+    const handleClick = () => {
+        initialize();
+    };
+
     document.body.style.backgroundImage = `url(${pictures})`;
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundAttachment = "fixed";
     document.body.style.backgroundPosition = "center";
+
     return (
-        <h2>Main</h2>
+        <div className="container mt-5">
+            <h1> Main Page</h1>
+            <h3>Инициализация данных в FireBase</h3>
+            <ul>
+                <li>Status: {status}</li>
+                <li>Progress: {progress}%</li>
+                {error && <li>Error: {error}</li>}
+            </ul>
+            <button className="btn btn-primary" onClick={handleClick}>
+                {" "}
+                Инициализировать
+            </button>
+        </div>
     );
 };
 
