03e6f116ffa15a95289d6bab44d80e577562ef16
VeraVK <pvk@git.one-touch.ru>
2022-06-09 13:35:04 +0300

    16. Авторизация и аутентификация. Часть 1

--- src/App.js
+++ src/App.js
@@ -8,23 +8,29 @@ import NotFound from "./components/page/notFound";
 import { ProfessionProvider } from "./hooks/useProfession";
 import { QualitiesProvider } from "./hooks/useQualities";
 import Users from "./layouts/users";
+import AuthProvider from "./hooks/useAuth";
+import AuthProviderLogIn from "./hooks/useAuthLogIn";
 
 function App() {
     return (
         <div>
-            <NavBar/>
-            <ProfessionProvider>
-                <QualitiesProvider>
-                    <Switch>
-                        <Route path="/" exact exactly component={Main}/>
-                        <Route path="/login/:type?" exactly component={Login}/>
-                        <Route path="/users/:userId?/:edit?" component={Users} />
-                        <Route path="/404" exactly component={NotFound}/>
-                        <Redirect from="/admin" to="/"/>
-                        <Redirect to="/404"/>
-                    </Switch>
-                </QualitiesProvider>
-            </ProfessionProvider>
+            <AuthProvider>
+                <AuthProviderLogIn>
+                    <NavBar/>
+                    <ProfessionProvider>
+                        <QualitiesProvider>
+                            <Switch>
+                                <Route path="/" exact exactly component={Main}/>
+                                <Route path="/login/:type?" exactly component={Login}/>
+                                <Route path="/users/:userId?/:edit?" component={Users} />
+                                <Route path="/404" exactly component={NotFound}/>
+                                <Redirect from="/admin" to="/"/>
+                                <Redirect to="/404"/>
+                            </Switch>
+                        </QualitiesProvider>
+                    </ProfessionProvider>
+                </AuthProviderLogIn>
+            </AuthProvider>
             <ToastContainer/>
         </div>
     );
