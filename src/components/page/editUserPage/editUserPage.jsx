a406f1d36692fbe1441b1bab71a21cb207702c86
VeraVK <pvk@git.one-touch.ru>
2022-06-18 10:12:08 +0300

    17. Авторизация и аутентификация. Часть 2 - полное решение дз

--- src/components/page/editUserPage/editUserPage.jsx
+++ src/components/page/editUserPage/editUserPage.jsx
@@ -1,45 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { useHistory, useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { validator } from "../../../utils/validator";
 import TextField from "../../common/form/textField";
+import SelectField from "../../common/form/selectField";
 import RadioField from "../../common/form/radioField";
+import MultiSelectField from "../../common/form/multiSelectField";
 import BackHistoryButton from "../../common/backButton";
-import pictures from "../../../statics/images/images.png";
+import { useAuth } from "../../../hooks/useAuth";
 import { useQualities } from "../../../hooks/useQualities";
 import { useProfessions } from "../../../hooks/useProfession";
-import { useUser } from "../../../hooks/useUsers";
-import SelectField from "../../common/form/selectField";
-import MultiSelectField from "../../common/form/multiSelectField";
-import { useAuth } from "../../../hooks/useAuth";
+import pictures from "../../../statics/images/images.png";
 
 const EditUserPage = () => {
-    const { userId, edit } = useParams();
-    const { currentUser } = useAuth();
-    const { updateUser } = useAuth();
     const history = useHistory();
-    const [isLoading, setIsLoading] = useState(false);
-    const { getUserById } = useUser();
-    const { qualities } = useQualities();
+    const [isLoading, setIsLoading] = useState(true);
+    const [data, setData] = useState();
+    const { currentUser, updateUserData } = useAuth();
+    const { qualities, isLoading: qualitiesLoading } = useQualities();
     const qualitiesList = qualities.map((q) => ({
         label: q.name,
         value: q._id
     }));
-    const { professions } = useProfessions();
+    const { professions, isLoading: professionLoading } = useProfessions();
     const professionsList = professions.map((p) => ({
         label: p.name,
         value: p._id
     }));
-    const user = getUserById(userId);
-    const newUser = {
-        ...user,
-        qualities: user.qualities.map((q) => {
-            for (let i = 0; i < qualitiesList.length; i++) {
-                if (qualitiesList[i].value === q) return qualitiesList[i];
-            }
-            return q;
-        })
-    };
-    const [dataUser, setDataUser] = useState(newUser);
     const [errors, setErrors] = useState({});
 
     document.body.style.backgroundImage = `url(${pictures})`;
@@ -48,24 +34,45 @@ const EditUserPage = () => {
         e.preventDefault();
         const isValid = validate();
         if (!isValid) return;
-        const newData = {
-            ...dataUser
-        };
-        try {
-            await updateUser(newData);
-            history.push(`/users/${dataUser._id}`);
-        } catch (error) {
-            setErrors(error);
-        }
+        await updateUserData({
+            ...data,
+            qualities: data.qualities.map((q) => q.value)
+        });
+
+        history.push(`/users/${currentUser._id}`);
     };
-
+    function getQualitiesListByIds(qualitiesIds) {
+        const qualitiesArray = [];
+        for (const qualId of qualitiesIds) {
+            for (const quality of qualities) {
+                if (quality._id === qualId) {
+                    qualitiesArray.push(quality);
+                    break;
+                }
+            }
+        }
+        return qualitiesArray;
+    }
+    const transformData = (data) => {
+        const result = getQualitiesListByIds(data).map((qual) => ({
+            label: qual.name,
+            value: qual._id
+        }));
+        return result;
+    };
     useEffect(() => {
-        setIsLoading(true);
-    }, []);
-
+        if (!professionLoading && !qualitiesLoading && currentUser && !data) {
+            setData({
+                ...currentUser,
+                qualities: transformData(currentUser.qualities)
+            });
+        }
+    }, [professionLoading, qualitiesLoading, currentUser, data]);
     useEffect(() => {
-        if (dataUser._id) setIsLoading(false);
-    }, [dataUser]);
+        if (data && isLoading) {
+            setIsLoading(false);
+        }
+    }, [data]);
 
     const validatorConfig = {
         email: {
@@ -82,90 +89,83 @@ const EditUserPage = () => {
             }
         }
     };
-    useEffect(() => validate(), [dataUser]);
-
+    useEffect(() => {
+        validate();
+    }, [data]);
     const handleChange = (target) => {
-        setDataUser((prevState) => ({
+        setData((prevState) => ({
             ...prevState,
             [target.name]: target.value
         }));
     };
     const validate = () => {
-        const errors = validator(dataUser, validatorConfig);
+        const errors = validator(data, validatorConfig);
         setErrors(errors);
         return Object.keys(errors).length === 0;
     };
     const isValid = Object.keys(errors).length === 0;
-
-    if (dataUser._id !== currentUser._id && edit === "edit") {
-        history.push(`/users/${currentUser._id}`);
-    };
-
-    console.log("dataUser", dataUser);
-
     return (
-        <>
-             {!isLoading && Object.keys(qualities).length > 0
-                ? <div className="container mt-5">
-                    <BackHistoryButton/>
-                    <div className="row">
-                        <div className="col-md-6 offset-md-3 shadow p-4">
-                            <form onSubmit={handleSubmit}>
-                                <TextField
-                                    label="Имя"
-                                    name="name"
-                                    value={dataUser.name}
-                                    onChange={handleChange}
-                                    error={errors.name}
-                                />
-                                <TextField
-                                    label="Электронная почта"
-                                    name="email"
-                                    value={dataUser.email}
-                                    onChange={handleChange}
-                                    error={errors.email}
-                                />
-                                <SelectField
-                                    label="Выбери свою профессию"
-                                    defaultOption="Choose..."
-                                    options={professionsList}
-                                    name="profession"
-                                    onChange={handleChange}
-                                    value={dataUser.profession}
-                                    error={errors.profession}
-                                />
-                                <RadioField
-                                    options={[
-                                        { name: "Male", value: "male" },
-                                        { name: "Female", value: "female" },
-                                        { name: "Other", value: "other" }
-                                    ]}
-                                    value={dataUser.sex}
-                                    name="sex"
-                                    onChange={handleChange}
-                                    label="Выберите ваш пол"
-                                />
-                                <MultiSelectField
-                                    options={qualitiesList}
-                                    onChange={handleChange}
-                                    defaultValue={dataUser.qualities}
-                                    name="qualities"
-                                    label="Выберите ваши качества"
-                                />
-                                <button
-                                    type="submit"
-                                    disabled={!isValid}
-                                    className="btn btn-primary w-100 mx-auto"
-                                >
-                                    Обновить
-                                </button>
-                            </form>
-                        </div>
-                    </div>
+        <div className="container mt-5">
+            <BackHistoryButton />
+            <div className="row">
+                <div className="col-md-6 offset-md-3 shadow p-4">
+                    {!isLoading && Object.keys(professions).length > 0 ? (
+                        <form onSubmit={handleSubmit}>
+                            <TextField
+                                label="Имя"
+                                name="name"
+                                value={data.name}
+                                onChange={handleChange}
+                                error={errors.name}
+                            />
+                            <TextField
+                                label="Электронная почта"
+                                name="email"
+                                value={data.email}
+                                onChange={handleChange}
+                                error={errors.email}
+                            />
+                            <SelectField
+                                label="Выбери свою профессию"
+                                defaultOption="Choose..."
+                                options={professionsList}
+                                name="profession"
+                                onChange={handleChange}
+                                value={data.profession}
+                                error={errors.profession}
+                            />
+                            <RadioField
+                                options={[
+                                    { name: "Male", value: "male" },
+                                    { name: "Female", value: "female" },
+                                    { name: "Other", value: "other" }
+                                ]}
+                                value={data.sex}
+                                name="sex"
+                                onChange={handleChange}
+                                label="Выберите ваш пол"
+                            />
+                            <MultiSelectField
+                                defaultValue={data.qualities}
+                                options={qualitiesList}
+                                onChange={handleChange}
+                                name="qualities"
+                                label="Выберите ваши качества"
+                            />
+                            <button
+                                type="submit"
+                                disabled={!isValid}
+                                className="btn btn-primary w-100 mx-auto"
+                            >
+                                Обновить
+                            </button>
+                        </form>
+                    ) : (
+                        "Loading..."
+                    )}
                 </div>
-                : "Loading..."
-            }
-        </>
+            </div>
+        </div>
     );
 };
 
--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -44,10 +44,13 @@ const AuthProvider = ({ children }) => {
             console.log(code, message);
             if (code === 400) {
                 switch (message) {
-                case "INVALID_PASSWORD":
-                    throw new Error("Email или пароль введены некорректно");
-                default:
-                    throw new Error("Слишком много попыток входа. Попробуйте позже");
+                    case "INVALID_PASSWORD":
+                        throw new Error("Email или пароль введены некорректно");
+
+                    default:
+                        throw new Error(
+                            "Слишком много попыток входа. Попробуйте позже"
+                        );
                 }
             }
         }
@@ -59,6 +62,14 @@ const AuthProvider = ({ children }) => {
     }
     function randomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    async function updateUserData(data) {
+        try {
+            const { content } = await userService.update(data);
+            setUser(content);
+        } catch (error) {
+            errorCatcher(error);
+        }
     }
     async function signUp({ email, password, ...rest }) {
         try {
@@ -94,41 +105,10 @@ const AuthProvider = ({ children }) => {
             }
         }
     }
-    async function updateUser({ ...rest }) {
-        try {
-            const { data } = await httpAuth.post(`accounts:update`, {
-                rest,
-                returnSecureToken: true
-            });
-            console.log("data", data);
-            console.log("rest11", rest);
-            setTokens(rest);
-            await createUser({
-                _id: rest.localId,
-                email: rest.email,
-                rate: rest.rate,
-                completedMeetings: rest.completedMeetings,
-                image: rest.image,
-                ...rest
-            });
-        } catch (error) {
-            errorCatcher(error);
-            const { code, message } = error.response.data.error;
-            console.log(code, message);
-            if (code === 400) {
-                if (message === "EMAIL_EXISTS") {
-                    const errorObject = {
-                        email: "Пользователь с таким Email уже существует"
-                    };
-                    throw errorObject;
-                }
-            }
-        }
-    }
     async function createUser(data) {
         try {
             const { content } = await userService.create(data);
-            console.log("content", content);
+            console.log(content);
             setUser(content);
         } catch (error) {
             errorCatcher(error);
@@ -162,7 +142,9 @@ const AuthProvider = ({ children }) => {
         }
     }, [error]);
     return (
-        <AuthContext.Provider value={{ signUp, logIn, currentUser, logOut, updateUser }}>
+        <AuthContext.Provider
+            value={{ signUp, logIn, currentUser, logOut, updateUserData }}
+        >
             {!isLoading ? children : "Loading..."}
         </AuthContext.Provider>
     );
